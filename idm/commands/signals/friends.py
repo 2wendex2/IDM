b4d055ad83d445e19be9a176d5d1f4f12c4cde37
Wendex <2wendex2@mail.ru>
2021-01-31 09:19:41 +0300

    Команда подружиться 2

--- idm/commands/signals/friends.py
+++ idm/commands/signals/friends.py
@@ -7,20 +7,20 @@ def add_self_friend(event: SignalEvent) -> str:
     friend_id = event.msg['from_id']
     try:
         event.api('friends.add', user_id=friend_id)
-        edit_message(event.api, event.chat.peer_id, event.msg['id'],
+        new_message(event.api, event.chat.peer_id,
             message="✅ Все отлично, запрос отправлен")
         return "ok"
     except VkApiResponseException as e:
         if e.error_code == 174:
-            edit_message(event.api, event.chat.peer_id, event.msg['id'],
+            new_message(event.api, event.chat.peer_id,
             message="❗ Невозможно добавить в друзья самого себя")
         elif e.error_code == 175:
-            edit_message(event.api, event.chat.peer_id, event.msg['id'],
+            new_message(event.api, event.chat.peer_id,
                 message="❗ Невозможно добавить в друзья пользователя, который занес Вас в свой черный список")
         elif e.error_code == 176:
-            edit_message(event.api, event.chat.peer_id, event.msg['id'],
+            new_message(event.api, event.chat.peer_id,
                 message="❗ Невозможно добавить в друзья пользователя, который занесен в Ваш черный список")
         else:
-            edit_message(event.api, event.chat.peer_id, event.msg['id'],
+            new_message(event.api, event.chat.peer_id,
                 message=f"❗ Невозможно добавить в друзья пользователя: {e.error_msg}")
         return "ok"
